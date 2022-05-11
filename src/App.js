46089e85b8e708c770e377ae0121ede4b8f73056
Jewel James <flakyhermit@protonmail.ch>
2022-05-11 17:25:11 +0530

    wip: added to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import StoryList from './StoryList'
+
 function App() {
+  const apiURL = "https://hacker-news.firebaseio.com/v0/topstories.json"
+  let id_list = []
+
+  fetch(apiURL)
+    .then(r => r.json())
+    .then(arr => {
+      id_list = arr
+    })
+
   return (
     <div className="App">
-      <header className="App-header"></header>
-      {/* <StoryList /> */}
+      <header>HN Clone</header>
+      <StoryList id_list={id_list}/>
     </div>
   );
 }
