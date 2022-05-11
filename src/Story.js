8e9bc9b79378e87caed8021511d616e0dabbee19
Jewel James <flakyhermit@protonmail.ch>
2022-05-11 17:17:24 +0530

    wip: Story component

--- src/Story.js
+++ src/Story.js
@@ -1,1 +1,25 @@
-function Story({ id }) {}
+import useState from 'react'
+
+function Story({ id }) {
+  const apiURL = `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+
+  const [story, setStory] = useState(undefined)
+
+  fetch(apiURL)
+    .then(r => r.jon())
+    .then(rObj => {
+      alert(rObj)
+      setStory(rObj)
+    })
+
+  return (
+    <>
+      <h3>{story.title}</h3>
+      <p>{story.by}</p>
+      <p>{story.score}</p>
+      <a href={story.url}/>
+    </>
+  )
+}
+
+export default Story
