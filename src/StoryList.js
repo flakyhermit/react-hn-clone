6140ec58eab7fc60618bc5ba26750de0d6084ca8
Jewel James <flakyhermit@protonmail.ch>
2022-05-11 18:05:04 +0530

    post index

--- src/StoryList.js
+++ src/StoryList.js
@@ -3,8 +3,9 @@ import Story from './Story'
 function StoryList({ id_list }) {
   return (
     <>
-      {id_list.map((id) => (
+      {id_list.map((id, index) => (
         <>
+          <div>{index + 1}</div>
           <Story id={id} />
         </>
       ))}
